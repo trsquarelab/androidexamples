01274e0be016720a11bb8be199f8c2cca48effee
chikkutechie <chikkutechie@users.noreply.github.com>
2016-02-26 16:40:24 +0900

    Update WeatherClient.java

--- DesignSupportLibrary/src/main/java/com/example/designsupportlibrary/weather/rest/WeatherClient.java
+++ DesignSupportLibrary/src/main/java/com/example/designsupportlibrary/weather/rest/WeatherClient.java
@@ -8,7 +8,7 @@ import retrofit.GsonConverterFactory;
 
 public class WeatherClient {
     private static final String Url = "http://api.openweathermap.org/";
-    private static final String AppId = "Your App Id Goes Here"; // Add your app id here
+    private static final String AppId = "44db6a862fba0b067b1930da0d769e98"; // Add your app id here
 
     private WeatherService mService;
     private Retrofit mRetrofit;
